43a4a48e46f198a5cd16fb33ab3c26f0c363a6f4
StilianKaridov <karidovstilian@gmail.com>
2022-10-03 16:06:52 +0300

    Implemented all task for geography database

--- MySQL/Subqueries_Аnd_JOINs_Exercise/Problem-Solutions.sql
+++ MySQL/Subqueries_Аnd_JOINs_Exercise/Problem-Solutions.sql
@@ -136,8 +136,10 @@ ORDER BY e.employee_id
 LIMIT 5;
 
 # 11. Min Average Salary
-SELECT AVG(salary) 'min_average_salary'
-FROM employees
+SELECT 
+    AVG(salary) 'min_average_salary'
+FROM
+    employees
 GROUP BY department_id
 ORDER BY min_average_salary
 LIMIT 1;
@@ -171,10 +173,38 @@ GROUP BY c.country_code
 ORDER BY mountain_range DESC;
 
 # 14. Countries with Rivers
+SELECT 
+    c.country_name, r.river_name
+FROM
+    countries c
+        LEFT JOIN
+    countries_rivers cr ON c.country_code = cr.country_code
+        LEFT JOIN
+    rivers r ON cr.river_id = r.id
+WHERE
+    c.continent_code = 'AF'
+ORDER BY c.country_name
+LIMIT 5;
 
-
-# 15. Continents and Currencies
-
+# 15. *Continents and Currencies
+SELECT 
+    continent_code,
+    currency_code,
+    COUNT(currency_code) AS currency_usage
+FROM
+    countries c1
+GROUP BY continent_code , currency_code
+HAVING currency_usage = (SELECT 
+        COUNT(currency_code) AS count
+    FROM
+        countries c2
+    WHERE
+        c2.continent_code = c1.continent_code
+    GROUP BY c2.currency_code
+    ORDER BY count DESC
+    LIMIT 1)
+    AND currency_usage > 1
+ORDER BY continent_code , currency_code;
 
 # 16. Countries Without Any Mountains
 SELECT 
@@ -186,3 +216,25 @@ FROM
     countries;
 
 # 17. Highest Peak and Longest River by Country
+SELECT 
+    country_name,
+    (SELECT 
+            MAX(p.elevation)
+        FROM
+            mountains_countries
+                LEFT JOIN
+            peaks AS p USING (mountain_id)
+        WHERE
+            country_code = c.country_code) AS highest_peak_elevation,
+    (SELECT 
+            MAX(r.length)
+        FROM
+            countries_rivers AS cr
+                LEFT JOIN
+            rivers AS r ON cr.river_id = r.id
+        WHERE
+            country_code = c.country_code) AS longest_river_length
+FROM
+    countries AS c
+ORDER BY highest_peak_elevation DESC , longest_river_length DESC , country_name
+LIMIT 5;
