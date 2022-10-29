030494c8016299917aba575178d90d7611a8b134
StilianKaridov <karidovstilian@gmail.com>
2022-10-29 17:36:49 +0300

    All tasks from IntroToHibernate Exercise

---
+++ SpringData/src/introToHibernate_Exercise/introToHibernate_Exercise/src/main/java/FindEmployeesByFirstName.java
@@ -0,0 +1,31 @@
+import entities.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Scanner;
+
+public class FindEmployeesByFirstName {
+
+    private static final String GET_EMPLOYEES_FIRST_NAME_STARTING_WITH = "select e from Employee e" +
+            " where e.firstName LIKE concat(:fn,'%')";
+    private static final String PRINT_FORMAT = "%s %s - %s - ($%.2f)%n";
+
+    public static void main(String[] args) {
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("soft_uni");
+        final EntityManager entityManager = emf.createEntityManager();
+
+        final String firstNameStartingLetters = new Scanner(System.in).nextLine();
+
+        entityManager.createQuery(GET_EMPLOYEES_FIRST_NAME_STARTING_WITH, Employee.class)
+                .setParameter("fn", firstNameStartingLetters)
+                .getResultList()
+                .forEach(e -> System.out.printf(PRINT_FORMAT,
+                        e.getFirstName(),
+                        e.getLastName(),
+                        e.getJobTitle(),
+                        e.getSalary()));
+
+        entityManager.close();
+    }
+}
---
+++ SpringData/src/introToHibernate_Exercise/introToHibernate_Exercise/src/main/java/IncreaseSalaries.java
@@ -0,0 +1,37 @@
+import entities.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class IncreaseSalaries {
+
+    private static final String PRINT_FORMAT = "%s %s ($%.2f)%n";
+    private static final String UPDATE_EMPLOYEES_SALARY_BY_DEPARTMENT = "update Employee e" +
+            " set e.salary = e.salary * 1.12" +
+            " where e.department.id IN (1, 2, 4, 11)";
+
+    private static final String GET_EMPLOYEES_WITH_UPDATED_SALARY = "select e from Employee e" +
+            " where e.department.id IN (1, 2, 4, 11)";
+
+    public static void main(String[] args) {
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("soft_uni");
+        final EntityManager entityManager = emf.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        entityManager.createQuery(UPDATE_EMPLOYEES_SALARY_BY_DEPARTMENT)
+                .executeUpdate();
+
+        entityManager.getTransaction().commit();
+
+        entityManager.createQuery(GET_EMPLOYEES_WITH_UPDATED_SALARY, Employee.class)
+                .getResultList()
+                .forEach(e -> System.out.printf(PRINT_FORMAT,
+                        e.getFirstName(),
+                        e.getLastName(),
+                        e.getSalary()));
+
+        entityManager.close();
+    }
+}
